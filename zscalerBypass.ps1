1a72f082141cb235cffd99d5368a46b18ef0e857
robeerto13 <44124152+robeerto13@users.noreply.github.com>
2018-11-27 09:43:59 -0600

    Update zscalerBypass.ps1

--- zscalerBypass.ps1
+++ zscalerBypass.ps1
@@ -1,6 +1,7 @@
 ﻿# Nothing fancy, just a simple bypass for the zscaler app. The app restarts these services as soon as they're killed 
 # so a loop is required to keep the services down. The loop will run until you manualy kill it. Requires PS running with
 # admin privs. Admin escalation should be trivial in most cases -Robert
+# make if simpler? "for ($i=1; $i -le 10; $i++){(get-process zsaservice).kill()}"
  
 for ($i=1; $i -le 10; $i){
 $service = Get-WmiObject -Class Win32_Service -Filter "Name LIKE 'zsaservice'" | Select-Object -ExpandProperty ProcessId
