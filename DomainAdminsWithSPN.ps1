0b60d4516d05fedcc7a6375873996ef71f96120d
robeerto13 <44124152+robeerto13@users.noreply.github.com>
2020-03-10 20:19:57 -0500

    Add files via upload

---
+++ DomainAdminsWithSPN.ps1
@@ -0,0 +1,8 @@
+﻿Import-Module C:\AD\Tools\ADModule-master\Microsoft.ActiveDirectory.Management.dll
+Import-Module C:\AD\Tools\ADModule-master\ActiveDirectory\ActiveDirectory.psd1
+$name = @()
+
+$name = Get-ADGroupMember "domain admins" | foreach {$_.samaccountname}
+foreach($n in $name){
+    get-aduser $n -Properties servicePrincipalName | select samaccountname,servicePrincipalName
+    }
