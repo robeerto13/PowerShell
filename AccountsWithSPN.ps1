0490f9cf9d32a55a3df5963109d7ea5deb654a3e
robeerto13 <44124152+robeerto13@users.noreply.github.com>
2020-03-10 20:59:13 -0500

    Add files via upload

---
+++ AccountsWithSPN.ps1
@@ -0,0 +1,7 @@
+# find all accounts with SPN for Kerberoasting
+# roboto@graybeardsec.com
+
+Import-Module C:\AD\Tools\ADModule-master\Microsoft.ActiveDirectory.Management.dll
+Import-Module C:\AD\Tools\ADModule-master\ActiveDirectory\ActiveDirectory.psd1 
+
+Get-ADUser -Filter {servicePrincipalName -ne "$null"} -Properties servicePrincipalName | select samaccountname,servicePrincipalName
