5fb3e83c05b0c3385413982be6b47a16fc85da76
robeerto13 <44124152+robeerto13@users.noreply.github.com>
2020-03-10 20:42:10 -0500

    Update DomainAdminsWithSPN.ps1

--- DomainAdminsWithSPN.ps1
+++ DomainAdminsWithSPN.ps1
@@ -1,9 +1,10 @@
 # find DA with SPN for Kerberoasting
-Import-Module C:\AD\Tools\ADModule-master\Microsoft.ActiveDirectory.Management.dll
+ Import-Module C:\AD\Tools\ADModule-master\Microsoft.ActiveDirectory.Management.dll
 Import-Module C:\AD\Tools\ADModule-master\ActiveDirectory\ActiveDirectory.psd1
 $name = @()
 
-$name = Get-ADGroupMember "domain admins" | foreach {$_.samaccountname}
+$name = Get-ADGroupMember "domain admins" -Recursive | foreach {$_.samaccountname}
 foreach($n in $name){
-    get-aduser $n -Properties servicePrincipalName | select samaccountname,servicePrincipalName
-    }
+    $spn = get-aduser $n -Properties servicePrincipalName | ?{$_.servicePrincipalName -ne $null} | select samaccountname,servicePrincipalName
+    $spn
+    } 
